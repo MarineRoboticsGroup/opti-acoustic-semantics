12ade6a1f3ffd4870f7489350596fc18eeaded45
Kurran Singh <singhk@mit.edu>
2024-08-12 17:53:15 -0400

    coloring scheme for viz

--- graph_matcher.py
+++ graph_matcher.py
@@ -11,7 +11,7 @@ from io import BytesIO
 import rospy
 import message_filters
 from semanticslam_ros.msg import ObjectsVector, ObjectVector
-
+from visualization_msgs.msg import Marker
 
 pygm.set_backend('pytorch') # set default backend for pygmtools
 _ = torch.manual_seed(1) # fix random seed
@@ -30,6 +30,11 @@ class GraphMatcher:
             self.fullgraph = ObjectsVector()
             self.fullgraph.deserialize(bytes)
             self.subgraph = self.fullgraph
+        with open("kitti_seq05_colors.txt", 'rb') as binary_file:
+            buf = BytesIO(binary_file.read())
+            bytes = buf.getvalue()
+            self.lm_colors = Marker()
+            self.lm_colors.deserialize(bytes)
             
     # def create_edge_features(node_positions):
     #     """
@@ -69,7 +74,7 @@ class GraphMatcher:
         # Extract nodes and edges from subgraph and fullgraph
         
         # Latent centroids are to be used as node features
-        subgraph_nodes = [obj.latent_centroid for obj in subgraph.objects][::3]
+        subgraph_nodes = [obj.latent_centroid for obj in subgraph.objects][15:20]
         fullgraph_nodes = [obj.latent_centroid for obj in fullgraph.objects]
         subgraph_nodes = torch.tensor(subgraph_nodes)
         fullgraph_nodes = torch.tensor(fullgraph_nodes)
@@ -78,8 +83,11 @@ class GraphMatcher:
         subgraph_points = [obj.geometric_centroid for obj in subgraph.objects]
         fullgraph_points = [obj.geometric_centroid for obj in fullgraph.objects]
 
-        subgraph_points = np.array([[point.x, point.y, point.z] for point in subgraph_points])[::3]
+        subgraph_points = np.array([[point.x, point.y, point.z] for point in subgraph_points])[15:20]
         fullgraph_points = np.array([[point.x, point.y, point.z] for point in fullgraph_points])
+        
+        colors = self.lm_colors.colors
+        colors = [(color.r, color.g, color.b, color.a) for color in colors]
 
         # Create adjacency matrices
         A1 = self.create_adjacency_matrix(subgraph_points)
@@ -95,21 +103,38 @@ class GraphMatcher:
         conn1, edge1 = pygm.utils.dense_to_sparse(A1)
         conn2, edge2 = pygm.utils.dense_to_sparse(A2)
 
+        # Visualize the subgraph and fullgraph
+        # sub
+        G1 = nx.from_numpy_array(A1.numpy())
+        pos1 = nx.spring_layout(G1)
+        
+        selected = [15, 16, 17, 18, 19]
+        X_gt = torch.eye(num_nodes2)[selected, :]
+
+        # full 
+        G2 = nx.from_numpy_array(A2.numpy())
+        pos2 = nx.spring_layout(G2)
+        color1 = ['#FF5733' for _ in range(num_nodes1)]
+        color2 = ['#FF5733' if _ in selected else '#1f78b4' for _ in range(num_nodes2)]
+        plt.figure(figsize=(8, 4))
+        plt.subplot(1, 2, 1)
+        plt.title('Subgraph 1')
+        plt.gca().margins(0.4)
+        nx.draw_networkx(G1, pos=pos1, node_color=color1)
+        plt.subplot(1, 2, 2)
+        plt.title('Graph 2')
+        nx.draw_networkx(G2, pos=pos2, node_color=color2)
+                
+        
         # Define the affinity function
         gaussian_aff = functools.partial(pygm.utils.gaussian_aff_fn, sigma=.001)
         
-        # Build affinity matrix
-        # K = pygm.utils.build_aff_mat(
-        #     node_feat1=subgraph_nodes, 
-        #     edge_feat1=edge1, 
-        #     connectivity1=conn1,
-        #     node_feat2=fullgraph_nodes, 
-        #     edge_feat2=edge2, 
-        #     connectivity2=conn2
-        # )
-        
+        # Build affinity matrix      
         K = pygm.utils.build_aff_mat(subgraph_nodes, edge1, conn1, fullgraph_nodes, edge2, conn2, None, None, None, None, edge_aff_fn=gaussian_aff)
-
+        # K = pygm.utils.build_aff_mat(None, edge1, conn1, None, edge2, conn2, None, None, None, None, edge_aff_fn=gaussian_aff)
+        plt.figure(figsize=(4, 4))
+        plt.title(f'Affinity Matrix (size: {K.shape[0]}$\\times${K.shape[1]})')
+        plt.imshow(K.numpy(), cmap='Blues')
 
         # print("A1:\n", A1)
         # print("A2:\n", A2)
@@ -120,8 +145,11 @@ class GraphMatcher:
         print(num_nodes1, num_nodes2)
         print(n1, n2)
         print("Affinity Matrix:\n", K.shape)
+        print(n1.dtype, n2.dtype, K.dtype)
 
-
+        # with torch.set_grad_enabled(False):
+        #     X = pygm.ngm(K, n1max=float(num_nodes1), n2max=float(num_nodes2), pretrain='voc')
+        #     X = pygm.hungarian(X)
         X = pygm.rrwm(K, n1, n2)
         
         plt.figure(figsize=(8, 4))
@@ -140,6 +168,22 @@ class GraphMatcher:
         # plt.subplot(1, 2, 2)
         # plt.title('Ground Truth Matching Matrix')
         # plt.imshow(X_gt.numpy(), cmap='Blues')
+        
+        plt.figure(figsize=(8, 4))
+        plt.suptitle(f'RRWM Matching Result')
+        ax1 = plt.subplot(1, 2, 1)
+        plt.title('Subgraph 1')
+        plt.gca().margins(0.4)
+        nx.draw_networkx(G1, pos=pos1, node_color=color1)
+        ax2 = plt.subplot(1, 2, 2)
+        plt.title('Graph 2')
+        nx.draw_networkx(G2, pos=pos2, node_color=color2)
+        for i in range(num_nodes1):
+            j = torch.argmax(X[i]).item()
+            con = ConnectionPatch(xyA=pos1[i], xyB=pos2[j], coordsA="data", coordsB="data",
+                                axesA=ax1, axesB=ax2, color="green" if X_gt[i,j] == 1 else "red")
+            plt.gca().add_artist(con)
+        plt.show()
 
 
 if __name__ == "__main__":
--- graph_saver.py
+++ graph_saver.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import rospy
 import message_filters
 from semanticslam_ros.msg import ObjectsVector, ObjectVector
+from visualization_msgs.msg import Marker
 
 class GraphSaver:
     """
@@ -10,27 +11,40 @@ class GraphSaver:
     """
 
     def __init__(self) -> None:        
-        #subgraph_topic = rospy.get_param("~subgraph_topic", "/landmark_features")
         fullgraph_topic = rospy.get_param("~fullgraph_topic", "/landmark_features")
-        
-        # self.subgraph_sub = message_filters.Subscriber(subgraph_topic, ObjectsVector, queue_size=1)
-        # self.fullgraph_sub = message_filters.Subscriber(fullgraph_topic, ObjectsVector, queue_size=1)
-        # self.sync = message_filters.ApproximateTimeSynchronizer(
-        #     (self.subgraph_sub, self.fullgraph_sub), 1, 0.025
-        # )
+        lm_colors_topic = rospy.get_param("~lm_colors_topic", "/landmarks")
+        self.fullgraph_sub = message_filters.Subscriber(fullgraph_topic, ObjectsVector, queue_size=1)
+        self.lm_colors_sub = message_filters.Subscriber(lm_colors_topic, Marker, queue_size=1)
+        self.sync = message_filters.ApproximateTimeSynchronizer(
+            (self.fullgraph_sub, self.lm_colors_sub), 1, 0.025
+        )
 
-        # self.sync.registerCallback(self.write_map_to_file)
-        self.fullgraph_sub = rospy.Subscriber(fullgraph_topic, ObjectsVector, self.write_map_to_file)
+        self.sync.registerCallback(self.write_color_map_to_file)
+        # self.fullgraph_sub = rospy.Subscriber(fullgraph_topic, ObjectsVector, self.write_map_to_file)
     
     def write_map_to_file(self, fullgraph: ObjectsVector) -> None:
+        buff = BytesIO()
+        fullgraph.serialize(buff)
+        serialized_bytes = buff.getvalue()
+        with open("zpool1_map2.txt", "wb") as binary_file:
+            # Write bytes to file
+            binary_file.write(serialized_bytes)
+
+    
+    def write_color_map_to_file(self, fullgraph: ObjectsVector, lm_colors: Marker) -> None:
         buff = BytesIO()
         fullgraph.serialize(buff)
         serialized_bytes = buff.getvalue()
         with open("kitti_map.txt", "wb") as binary_file:
             # Write bytes to file
             binary_file.write(serialized_bytes)
-
-        # p2.deserialize(serialized_bytes)
+            
+        buff2 = BytesIO()
+        lm_colors.serialize(buff2)
+        serialized_bytes2 = buff2.getvalue()
+        with open("kitti_seq05_colors.txt", "wb") as binary_file2:
+            # Write bytes to file
+            binary_file2.write(serialized_bytes2)
         
 if __name__ == "__main__":
     rospy.init_node("graph_saver")
